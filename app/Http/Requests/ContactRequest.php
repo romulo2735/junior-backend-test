4a0c3057cb302edcd528b6031f90e155fc7bb01d
Romulo Sousa <romulosousa27@hotmail.com>
2025-07-06 20:53:07 -0300

    feat: validação de formulário

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
@@ -9,12 +10,16 @@ class ContactRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
+        $contactId = $this->route('contact')?->id;
+
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:contacts,email' . ($contactId ? ",$contactId" : ''),
+            'phone' => 'required|min:10',
         ];
     }
 }
