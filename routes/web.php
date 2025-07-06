24d820591ebf0e465782ea1f68b2b112bde0af43
Romulo Sousa <romulosousa27@hotmail.com>
2025-07-06 20:54:25 -0300

    feat: criando as rotas do gerenciamento de contatos

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
-use App\Http\Controllers\CreateContactController;
+use App\Http\Controllers\Contacts\{
+    ListContactsController,
+    CreateContactController,
+    StoreContactController,
+    EditContactController,
+    UpdateContactController,
+    DeleteContactController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/contacts', CreateContactController::class);
+Route::prefix('contacts')->name('contacts.')->group(function () {
+    Route::get('/', ListContactsController::class)->name('index');
+    Route::get('/create', CreateContactController::class)->name('create');
+    Route::post('/', StoreContactController::class)->name('store');
+    Route::get('/{contact}/edit', EditContactController::class)->name('edit');
+    Route::put('/{contact}', UpdateContactController::class)->name('update');
+    Route::delete('/{contact}', DeleteContactController::class)->name('destroy');
+});
